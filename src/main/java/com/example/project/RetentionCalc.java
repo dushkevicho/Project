6525be77851373b69ab384b0d5c963a7780089e8
Orest Dushkevich <dushkevicho@mymacewan.ca>
2022-08-26 22:43:09 -0600

    validateDate() finished started periodIntervalCalculator()

--- src/main/java/com/example/project/RetentionCalc.java
+++ src/main/java/com/example/project/RetentionCalc.java
@@ -2,36 +2,106 @@ package com.example.project;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class RetentionCalc {
 
         
         private ArrayList<Client> validClients = new ArrayList<>();
+        private int MAX_PERIODS = 10;           // hardcoded maximum number of periods
         private Calendar fromDate = null;       // if null do all data availible for contractor
+        private Calendar fromDatePeriod = null;
         private Calendar toDate = null;         // if null do all data availible for contractor
+        private Calendar toDatePeriod = null;
         private Calendar earliestDate = null;    //calculated from validClients
         private Calendar latestDate = null;     //calculated from validClients
-        private Calendar todaysDate = null;
+        private Calendar todayDate = null;
         private int daysSinceLastSessionMax = 30; //days since last session to be considered active
-        private int startOfWeek = 1;        // 1 is sunday as default
+        private int startOfWeek = Calendar.SUNDAY;        // 1 is sunday as default
         private int periodLength = 1;       // default is a period of 1 week
-        private int minSessionsForRetained = 0; // number of sessions
-        private int CalculatedPeriods = 0;   // needs to calculate to at least 1 period
+        private int minSessionsForRetained = 2; // number of sessions
+
+
+
+    private int calculatedPeriods = 0;   // needs to calculate to at least 1 period
+        private ArrayList<Period> periods = new ArrayList<>();
+
+    /**
+     * this function calculates the parameters
+     * @return
+     */
+    public String calculateParameters() {
+
+        // checks to see if parameters are set correctly
+        // returns  an error message if they are not
+        String ValidateOptions = validateOptions();
+        if (ValidateOptions != null) {
+            return ValidateOptions;
+        }
+        // calculate the periods
+        periodIntervalCalculator();
+               
 
         
-        public String ValidateOptions(){
-            
-            
-            
-            if (validateDate() != null) {
-                return validateDate();
-            } 
+
+        return null;
+    }
+
+    /**
+     * This function calculates the start of the first period and 
+     * the amount of periods in the range
+     */
+    private void periodIntervalCalculator() {
+
+        // determine the first day of the period to calculate from
+        int dayOfWeek = fromDate.get(Calendar.DAY_OF_WEEK);
+        int deltaDays = startOfWeek - dayOfWeek;
+        fromDatePeriod = (Calendar) fromDate.clone();
+        fromDatePeriod.add(Calendar.DAY_OF_MONTH, deltaDays);
+
+
+
+
+
+
+        long days = TimeUnit.MILLISECONDS.toDays( Math.abs(fromDatePeriod.getTimeInMillis() - toDate.getTimeInMillis()));
+        System.out.println(days);
+
+        //TODO this rounds down so that the last period isn't a partial week
+        calculatedPeriods = (int) days / (7 * periodLength);
+
+
+        toDatePeriod = (Calendar) fromDatePeriod.clone();
+
+        for (int i = 0; i < calculatedPeriods; i++) {
+            System.out.println(toDatePeriod.getTime());
+            toDatePeriod.add(Calendar.DATE, periodLength * 7);
+
+        }
+
+        
+        
+    }
+    
+    
+
+
+    /**
+     * this function validates all the options for the retention calculator
+     * @return String the validation error message
+     */
+    public String validateOptions(){
+
+        String validateDate = validateDate();
+            if (validateDate != null) {
+                return validateDate;
+            }
+
+            periodIntervalCalculator();
 
             //TODO validate that they are at least one period apart
 
             return null;
-
-
 
         }
 
@@ -39,7 +109,6 @@ public class RetentionCalc {
         /**
         validates the ToDate and fromDate, returns a string describing the problem if invalid
         */
-
         private String validateDate() {
 
             // validates that fromDate was entered
@@ -52,27 +121,33 @@ public class RetentionCalc {
                 return "No to date entered";
             }
 
-            // valiadtaes that toDate is before today 
-            if (this.toDate.compareTo(this.todaysDate) > 0 ) {
+            // validates that toDate is before today
+            if (this.toDate.compareTo(this.todayDate) > 0 ) {
                 return "To date in the future, must be before today";
             }
 
-            // valiadtaes that fromDate is before today 
-            if (this.fromDate.compareTo(this.todaysDate) > 0 ) {
+            // validates that fromDate is before today
+            if (this.fromDate.compareTo(this.todayDate) > 0 ) {
                 return "To date in the future, must be before today";
             }
-            
 
+            // validates that fromDate is before toDate
             if (this.fromDate.compareTo(this.toDate) > 0) {
                 return "From date cannot be after to date";
-            } 
+            }
+
+            // validates that toDate does not fall on the same week to date
+            if (toDate.get(Calendar.WEEK_OF_YEAR) == todayDate.get(Calendar.WEEK_OF_YEAR)
+                    && toDate.get(Calendar.YEAR) == todayDate.get(Calendar.YEAR)) {
+                return "To date Can't be same week as today";
+            }
 
             return null;
         }
 
         public RetentionCalc(Contractor contractor) {
             processValidClients(contractor);
-            this.todaysDate = Calendar.getInstance();
+            this.todayDate = Calendar.getInstance();
         }    
 
         // TODO add first session date and last session date in Client
@@ -102,17 +177,66 @@ public class RetentionCalc {
             }
 
         }
+    public int getCalculatedPeriods() {
+        return calculatedPeriods;
+    }
+
+    public Calendar getFromDatePeriod() {
+        return fromDatePeriod;
+    }
+
+        private class Period {
+            int periodNumber;
+            Calendar periodStartDate;
+            Calendar periodEndDate;
+            int newClients = 0;
+            int retainedClients = 0;
+            int retainedPercentage = 0;
+
+
+
+            public Period (int periodNumber) {
+                this.periodNumber = periodNumber;
+            }
+
+            public void setPeriodStartDate(Calendar periodStartDate) {
+                this.periodStartDate = periodStartDate;
+            }
+
+            public void setPeriodEndDate(Calendar periodEndDate) {
+                this.periodEndDate = periodEndDate;
+            }
+
+            public void addNewClients() {
+                newClients++;
+            }
+
+            public void addRetainedClients() {
+                retainedClients++;
+                calculatePercent();
+            }
+
+            private void calculatePercent() {
+                retainedPercentage = (int) 100 * retainedClients / newClients;
+            }
+        }
 
         
 
         /* this method sets toDate and validates it return an error string if invalid
          * othewise returns a null*/
-        public void setToDate(Calendar toDate) {
-            this.toDate = toDate;
+        public void setToDate(int day, int month, int year) {
+            this.toDate = Calendar.getInstance();
+            this.toDate.set(year, month, day, 23, 59);
+            this.toDate.set(Calendar.SECOND, 59);
+            this.toDate.set(Calendar.MILLISECOND, 999);
         }
 
-        public void setFromDate(Calendar fromDate) {
-            Calendar todayDate = fromDate;   
+        public void setFromDate(int day, int month, int year) {
+            this.fromDate = Calendar.getInstance();
+            this.fromDate.set(year, month, day, 00, 00);
+            this.fromDate.set(Calendar.SECOND, 0);
+            this.fromDate.set(Calendar.MILLISECOND, 0);
         }
 
         public void setPeriodLength (int periodLength) {
@@ -120,7 +244,7 @@ public class RetentionCalc {
         }
 
         
-        void setStartofWeek(int startOfWeek) {
+        void setStartOfWeek(int startOfWeek) {
             this.startOfWeek = startOfWeek;
         }
 
--- src/test/java/com/example/project/RetentionCalcTest.java
+++ src/test/java/com/example/project/RetentionCalcTest.java
@@ -1,8 +1,102 @@
 package com.example.project;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RetentionCalcTest {
+    String test4Filepath = "..\\Final Project\\test4.csv";
 
+    /**
+     * this test tests the date validation
+     */
+    @Test
+    void validateDateTest() {
+        GetDataDAO getDataDAO = new CsvGetDataDAO();
+        AllClientSessions allClientSessions = getDataDAO.getAllData(test4Filepath);
+
+        // test to see that all entries were imported
+        int allSessionsSize = allClientSessions.AllClientSessionsSizeRaw();
+        assertEquals(32, allSessionsSize);
+
+        // sort the data
+        allClientSessions.SortRaw();
+
+        Contractor contractor582 =allClientSessions.getContractorFromName("contractor name 582");
+        RetentionCalc retention582 = new RetentionCalc(contractor582);
+
+
+        String error1 = retention582.validateOptions();
+        assertEquals("No from date entered", error1);
+
+        retention582.setFromDate(1, Calendar.JANUARY, 2020);
+        String error2 = retention582.validateOptions();
+        assertEquals("No to date entered", error2);
+
+        retention582.setToDate(1, 1, 2023);
+        String error3 = retention582.validateOptions();
+        assertEquals("To date in the future, must be before today", error3);
+
+        retention582.setToDate(3, 3, 2018);
+        String error4 = retention582.validateOptions();
+        assertEquals("From date cannot be after to date", error4);
+
+        // this test will probably not work on a sunday
+        Calendar today = Calendar.getInstance();
+        retention582.setToDate(today.get(Calendar.DAY_OF_MONTH)-1, today.get(Calendar.MONTH), today.get(Calendar.YEAR));
+        String error4a = retention582.validateOptions();
+        assertEquals("To date Can't be same week as today", error4a);
+
+
+        // date range is valid
+        retention582.setToDate(2, Calendar.FEBRUARY, 2020);
+        String error5 = retention582.validateOptions();
+        assertNull(error5);
+
+        }
+
+    @Test
+    void testPeriodCalculator() {
+
+        GetDataDAO getDataDAO = new CsvGetDataDAO();
+        AllClientSessions allClientSessions = getDataDAO.getAllData(test4Filepath);
+
+        // sort the data
+        allClientSessions.SortRaw();
+
+        Contractor contractor582 =allClientSessions.getContractorFromName("contractor name 582");
+        RetentionCalc retention582 = new RetentionCalc(contractor582);
+
+        // test that the amount of periods with defaults get calculated correctly
+        retention582.setFromDate(1, Calendar.JULY, 2022);
+        retention582.setToDate(2, Calendar.AUGUST, 2022);
+        String error1 = retention582.validateOptions();
+        assertNull(error1);
+        assertEquals(26,retention582.getFromDatePeriod().get(Calendar.DAY_OF_MONTH));
+        assertEquals(5, retention582.getCalculatedPeriods());
+
+        // test that different start of week works
+        retention582.setStartOfWeek(Calendar.SATURDAY);
+        error1 = retention582.validateOptions();
+        assertNull(error1);
+        assertEquals(2, retention582.getFromDatePeriod().get(Calendar.DAY_OF_MONTH));
+        assertEquals(4, retention582.getCalculatedPeriods());
+
+        //test different length of period
+        retention582.setStartOfWeek(Calendar.SUNDAY);
+        retention582.setPeriodLength(2);
+        error1 = retention582.validateOptions();
+        assertNull(error1);
+        assertEquals(2,retention582.getCalculatedPeriods());
+
+
+
+
+
+
+
+    }
 
 }
